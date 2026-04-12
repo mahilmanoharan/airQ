6d6917446e986eb3b080b0419f1d1174209a5a31
Mahil <mahil@unc.edu>
2026-04-12 09:46:55 -0400

    Initial Commit

---
+++ airQ/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  airQ
+//
+//  Created by Mahil Manoharan on 4/12/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ airQ/airQApp.swift
@@ -0,0 +1,17 @@
+//
+//  airQApp.swift
+//  airQ
+//
+//  Created by Mahil Manoharan on 4/12/26.
+//
+
+import SwiftUI
+
+@main
+struct airQApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
